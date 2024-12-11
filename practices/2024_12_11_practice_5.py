5e6022172eb55e318f615be389ce6a4697adb4a6
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-11 17:49:15 -0600

    Update: testing code

---
+++ practices/2024_12_11_practice_5.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-11
+# Topic: data-structures
+# Practice #5
+
+def solution_5():
+    """Practice problem for data-structures"""
+    result = []
+    for i in range(5):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_5())
