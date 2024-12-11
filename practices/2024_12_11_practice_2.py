56fcd484d08fd8cb342c48f01f067df613d02531
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-11 13:07:32 -0600

    Practice: documentation

---
+++ practices/2024_12_11_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-11
+# Topic: code-refactoring
+# Practice #2
+
+def solution_2():
+    """Practice problem for code-refactoring"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
