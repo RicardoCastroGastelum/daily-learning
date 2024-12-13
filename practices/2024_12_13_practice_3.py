69dc116cd03707d42dbe0ff4302aab45bdfde149
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-13 16:28:15 -0600

    Solve: testing problem

---
+++ practices/2024_12_13_practice_3.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-13
+# Topic: api-practice
+# Practice #3
+
+def solution_3():
+    """Practice problem for api-practice"""
+    result = []
+    for i in range(3):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_3())
