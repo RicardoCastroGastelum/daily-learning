128a714e3006ff4c921236db0a25bee119a6ff93
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-13 13:58:52 -0600

    Add: testing solution

---
+++ practices/2024_12_13_practice_4.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-13
+# Topic: database-queries
+# Practice #4
+
+def solution_4():
+    """Practice problem for database-queries"""
+    result = []
+    for i in range(4):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_4())
