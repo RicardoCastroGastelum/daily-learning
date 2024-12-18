e40da6cd2d0ae9839b6246c7971f7712205d72f7
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-18 09:43:54 -0600

    Complete: data-structures exercise

---
+++ practices/2024_12_18_practice_1.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-18
+# Topic: code-refactoring
+# Practice #1
+
+def solution_1():
+    """Practice problem for code-refactoring"""
+    result = []
+    for i in range(1):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_1())
