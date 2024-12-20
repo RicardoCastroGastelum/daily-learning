59dc250e40e3c7b62daae749758906662f96fbda
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-20 11:33:32 -0600

    Update: leetcode code

---
+++ practices/2024_12_20_practice_6.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-20
+# Topic: algorithms
+# Practice #6
+
+def solution_6():
+    """Practice problem for algorithms"""
+    result = []
+    for i in range(6):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_6())
