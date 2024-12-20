d6ad9272472d316b702a9f3dee282dc59b1e9acd
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-20 11:33:03 -0600

    Practice: python-basics

---
+++ practices/2024_12_20_practice_4.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-20
+# Topic: algorithms
+# Practice #4
+
+def solution_4():
+    """Practice problem for algorithms"""
+    result = []
+    for i in range(4):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_4())
