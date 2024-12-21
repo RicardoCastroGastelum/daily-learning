2cdc5616b454dbf78e79ec6032ecedc207791a12
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2024-12-20 22:42:24 -0600

    Practice: debugging

---
+++ practices/2024_12_20_practice_1.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2024-12-20
+# Topic: testing
+# Practice #1
+
+def solution_1():
+    """Practice problem for testing"""
+    result = []
+    for i in range(1):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_1())
