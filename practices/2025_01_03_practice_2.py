012c02d3dbb01f5fc6e61d8935c5ad574dcf90bc
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-03 15:05:06 -0600

    Solve: performance-optimization problem

---
+++ practices/2025_01_03_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-03
+# Topic: data-structures
+# Practice #2
+
+def solution_2():
+    """Practice problem for data-structures"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
