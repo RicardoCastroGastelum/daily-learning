51e6ac26e4803950e27aad8243976b159ba3b772
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-03 20:23:26 -0600

    Practice: leetcode

---
+++ practices/2025_01_03_practice_1.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-03
+# Topic: database-queries
+# Practice #1
+
+def solution_1():
+    """Practice problem for database-queries"""
+    result = []
+    for i in range(1):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_1())
