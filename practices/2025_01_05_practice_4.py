d19991147e4c574de318065657aabcb1f9741f61
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-05 19:48:11 -0600

    Solve: database-queries problem

---
+++ practices/2025_01_05_practice_4.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-05
+# Topic: leetcode
+# Practice #4
+
+def solution_4():
+    """Practice problem for leetcode"""
+    result = []
+    for i in range(4):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_4())
