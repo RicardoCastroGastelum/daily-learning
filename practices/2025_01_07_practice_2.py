b81839e6d56ca48d3a97c18ece6cbf7865b3219a
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-07 13:12:49 -0600

    Practice: web-scraping

---
+++ practices/2025_01_07_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-07
+# Topic: api-practice
+# Practice #2
+
+def solution_2():
+    """Practice problem for api-practice"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
