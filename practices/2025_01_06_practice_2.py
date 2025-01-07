3bde13e746745d4e253781e4dc15e18821037760
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-06 18:13:13 -0600

    Add: code-refactoring solution

---
+++ practices/2025_01_06_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-06
+# Topic: performance-optimization
+# Practice #2
+
+def solution_2():
+    """Practice problem for performance-optimization"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
