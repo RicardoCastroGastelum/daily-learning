50556bfcd6a468c797441c74ba74eeede8daa74e
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-08 12:13:45 -0600

    Practice: performance-optimization

---
+++ practices/2025_01_08_practice_1.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-08
+# Topic: documentation
+# Practice #1
+
+def solution_1():
+    """Practice problem for documentation"""
+    result = []
+    for i in range(1):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_1())
