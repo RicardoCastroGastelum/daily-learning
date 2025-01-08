9e94370ae13264c39df3bbed7b32a84a20048e18
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-07 20:15:48 -0600

    Solve: algorithms problem

---
+++ practices/2025_01_07_practice_3.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-07
+# Topic: code-refactoring
+# Practice #3
+
+def solution_3():
+    """Practice problem for code-refactoring"""
+    result = []
+    for i in range(3):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_3())
