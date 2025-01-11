30364fcfffda6660a5f8d306dc85722aab3019b3
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-11 15:47:15 -0600

    Add: documentation solution

---
+++ practices/2025_01_11_practice_6.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-11
+# Topic: documentation
+# Practice #6
+
+def solution_6():
+    """Practice problem for documentation"""
+    result = []
+    for i in range(6):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_6())
