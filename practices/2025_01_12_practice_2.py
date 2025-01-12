8cd28989fb7edb3860854b8fd8d63f8d442eb6ba
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-12 09:52:19 -0600

    Practice: data-structures

---
+++ practices/2025_01_12_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-12
+# Topic: web-scraping
+# Practice #2
+
+def solution_2():
+    """Practice problem for web-scraping"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
