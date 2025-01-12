54a62cf0981e3b8fcf0ba0eeb6a068edb516194b
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-11 21:08:34 -0600

    Complete: code-refactoring exercise

---
+++ practices/2025_01_11_practice_4.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-11
+# Topic: testing
+# Practice #4
+
+def solution_4():
+    """Practice problem for testing"""
+    result = []
+    for i in range(4):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_4())
