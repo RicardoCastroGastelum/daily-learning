989ac9ab95545a8f10ac7adf4c52026b19ad75d6
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-12 15:58:25 -0600

    Complete: leetcode exercise

---
+++ practices/2025_01_12_practice_3.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-12
+# Topic: documentation
+# Practice #3
+
+def solution_3():
+    """Practice problem for documentation"""
+    result = []
+    for i in range(3):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_3())
