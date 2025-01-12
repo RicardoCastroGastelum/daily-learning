d12fe16ba45363b8097d9cb7c20381eff05c360c
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-12 11:41:01 -0600

    Complete: testing exercise

---
+++ practices/2025_01_12_practice_5.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-12
+# Topic: leetcode
+# Practice #5
+
+def solution_5():
+    """Practice problem for leetcode"""
+    result = []
+    for i in range(5):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_5())
