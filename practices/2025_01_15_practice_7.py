22353e2e04510573f5aba109326107481c9143ac
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-15 15:13:36 -0600

    Complete: api-practice exercise

---
+++ practices/2025_01_15_practice_7.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-15
+# Topic: data-structures
+# Practice #7
+
+def solution_7():
+    """Practice problem for data-structures"""
+    result = []
+    for i in range(7):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_7())
