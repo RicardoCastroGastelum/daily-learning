8a4decdaea0b38a10ca304b4eba785a60f30d523
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-15 22:31:14 -0600

    Update: documentation code

---
+++ practices/2025_01_15_practice_6.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-15
+# Topic: python-basics
+# Practice #6
+
+def solution_6():
+    """Practice problem for python-basics"""
+    result = []
+    for i in range(6):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_6())
