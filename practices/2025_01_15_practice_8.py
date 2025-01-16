a3f1c68c4dc73cdf7bf7b1e1ee55c343a2d55bbf
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-15 18:31:53 -0600

    Add: algorithms solution

---
+++ practices/2025_01_15_practice_8.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-15
+# Topic: python-basics
+# Practice #8
+
+def solution_8():
+    """Practice problem for python-basics"""
+    result = []
+    for i in range(8):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_8())
