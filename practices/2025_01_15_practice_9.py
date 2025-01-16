c4f812ba3669a97e3b10118b2a42949f02be2801
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-15 20:49:24 -0600

    Solve: testing problem

---
+++ practices/2025_01_15_practice_9.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-15
+# Topic: documentation
+# Practice #9
+
+def solution_9():
+    """Practice problem for documentation"""
+    result = []
+    for i in range(9):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_9())
