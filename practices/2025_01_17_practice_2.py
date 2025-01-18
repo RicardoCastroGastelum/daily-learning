d9dfd651f19538f75b6b6abee0cb3435ed6767d2
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-17 22:36:11 -0600

    Add: database-queries solution

---
+++ practices/2025_01_17_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-17
+# Topic: debugging
+# Practice #2
+
+def solution_2():
+    """Practice problem for debugging"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
