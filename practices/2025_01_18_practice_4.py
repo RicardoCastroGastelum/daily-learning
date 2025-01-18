00a420b521f97d696ca95fe1f615acfb4d0a24cf
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-18 12:25:38 -0600

    Add: documentation solution

---
+++ practices/2025_01_18_practice_4.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-18
+# Topic: debugging
+# Practice #4
+
+def solution_4():
+    """Practice problem for debugging"""
+    result = []
+    for i in range(4):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_4())
