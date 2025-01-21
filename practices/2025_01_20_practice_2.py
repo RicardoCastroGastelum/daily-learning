965625101aaee62759172f72bfa4bcc9a992233d
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-20 21:27:42 -0600

    Update: documentation code

---
+++ practices/2025_01_20_practice_2.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-20
+# Topic: leetcode
+# Practice #2
+
+def solution_2():
+    """Practice problem for leetcode"""
+    result = []
+    for i in range(2):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_2())
