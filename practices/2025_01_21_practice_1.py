f922e6d090feeebffd160b4da605837667589f6b
RicardoCastroGastelum <bloorecordsmx@gmail.com>
2025-01-21 20:40:44 -0600

    Update: debugging code

---
+++ practices/2025_01_21_practice_1.py
@@ -0,0 +1,13 @@
+# Daily Practice - 2025-01-21
+# Topic: data-structures
+# Practice #1
+
+def solution_1():
+    """Practice problem for data-structures"""
+    result = []
+    for i in range(1):
+        result.append(i * 2)
+    return result
+
+if __name__ == "__main__":
+    print(solution_1())
